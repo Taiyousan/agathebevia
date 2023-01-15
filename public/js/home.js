48da5c383720d17c5a9d154d30b97fa8be575886
Taiyousan <shamsbenhamou@gmail.com>
2023-01-15 21:32:45 +0100

    carousel home

---
+++ public/js/home.js
@@ -0,0 +1,12 @@
+// const leftArrow = document.querySelector('.left-container');
+// const rightArrow = document.querySelector('.right-container');
+// const landing = document.querySelector('.home-landing');
+// const test = document.getElementById('test');
+
+// console.log(test)
+// console.log('test')
+// landing.addEventListener('click', function(){
+//     let i = 0
+//     console.log('ok')
+//     landing.style.backgroundImage = 'url({{ asset("photos/photo6.jpg") }})';
+// })
