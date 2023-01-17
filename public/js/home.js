b8f180c43dbe8388819599a3a7aa44d3984d4cae
Taiyousan <shamsbenhamou@gmail.com>
2023-01-17 14:45:04 +0100

    correction animation menu categories

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,22 @@
-// const leftArrow = document.querySelector('.left-container');
-// const rightArrow = document.querySelector('.right-container');
-// const landing = document.querySelector('.home-landing');
-// const test = document.getElementById('test');
+// animation au clic sur les catégories de l'accueil
 
-// console.log(test)
-// console.log('test')
-// landing.addEventListener('click', function(){
-//     let i = 0
-//     console.log('ok')
-//     landing.style.backgroundImage = 'url({{ asset("photos/photo6.jpg") }})';
-// })
+const categories = document.querySelectorAll('.category');
+const menuCategorie = document.querySelector('.home-menu');
 
+categories.forEach(category => {
+category.addEventListener('click', function(){
+    // anim au click
+    category.querySelector('p').style.transform = "translate(-30vw)";
+    category.style.backgroundSize = "120%";
+
+    //anim reset si on clique ailleurs
+    menuCategorie.addEventListener('click', function(e){   
+        if (category.contains(e.target)){
+        } else{
+            category.querySelector('p').style.transform = "translate(0vw)";
+            category.style.backgroundSize = "100%";
+        }
+      });
+})
+}
+)
